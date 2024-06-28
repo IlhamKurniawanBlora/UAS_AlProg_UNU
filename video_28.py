099821877b1314a64ac98cdcb6d6a7cba8e74ff6
ilhamkurniawanjateng <ilhamkurniawanjateng@gmail.com>
2024-06-28 14:28:38 +0000

    update video_28

--- video_28.py
+++ video_28.py
@@ -19,3 +19,51 @@ while True:
 
     if count > sisi:
         break
+print("akhir dari program")
+# 3. hanya ganjil saja
+
+print("awal program")
+count = 1
+
+while True:
+    
+    if (count%2):
+        # Print jika ganjil
+        print("*"*count)
+        count += 1
+    else:
+        # akan kembali ke atas jika ganjil
+        count += 1
+        continue
+    # akan break jika count melebihi sisi
+    if count > sisi:
+        break
+ 
+print("akhir dari program")
+
+# Suggested code may be subject to a license. Learn more: ~LicenseLog:2181095959.
+print("akhir dari program")
+# 4. hanya ganjil saja
+
+print("awal program")
+count = 1
+spasi = int(sisi/2)
+
+while True:
+    
+    if (count%2):
+        # Print jika ganjil
+        print(" "*spasi,"+"*count)
+        spasi -= 1
+        count += 1
+    else:
+        # akan kembali ke atas jika ganjil
+        count += 1
+        continue
+    # akan break jika count melebihi sisi
+    if count > sisi:
+        break
+ 
+print("akhir dari program")
+
+#
